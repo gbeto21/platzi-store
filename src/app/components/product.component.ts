dc5a5bd78829ac196f4b9c1ba0991e937640b906
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-12 08:42:46 -0500

    The life cicle interfaces of an component were implemented. The class: ''Ciclo de vida de los componentes.' has finished.

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,11 +1,42 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core'
+import {
+    Component, 
+    Input, 
+    Output, 
+    EventEmitter, 
+    OnChanges, 
+    SimpleChanges, 
+    OnInit, 
+    DoCheck, 
+    OnDestroy} from '@angular/core'
 import {Product} from '../product.model'
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent{
+export class ProductComponent implements OnInit, DoCheck, OnDestroy{
+    
+    // ngOnChanges(changes: SimpleChanges): void {
+    //     //Detecta el estado anterior y el nuevo.
+    // }
+
+    ngOnInit(): void {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        
+    }
+
+    ngDoCheck(): void {
+        //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+        //Add 'implements DoCheck' to the class.
+        
+    }
+
+    ngOnDestroy(): void {
+        //Called once, before the instance is destroyed.
+        //Add 'implements OnDestroy' to the class.
+        
+    }
 
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
