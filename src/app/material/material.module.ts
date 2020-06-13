12ef67e1cfce532d720abcd96b5a27d2e0f566c9
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-13 13:16:06 -0500

    Styles and classes were added to the product template. The class: 'Estilos a product-card' has finished.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge'
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule
   ],
   exports: [
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
