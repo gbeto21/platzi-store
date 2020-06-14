671df351d9445f8365745516c7e26069fe091882
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-14 08:07:44 -0500

    The funtionality of upload a new image was develop. The class: 'Subiendo una imagen a firebase storage' has finished.

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { AuthService } from "./core/services/auth.service";
@@ -9,7 +9,10 @@ import { AuthService } from "./core/services/auth.service";
 })
 export class AdminGuard implements CanActivate {
 
-  constructor(private authService: AuthService){
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){
     
   }
 
@@ -17,8 +20,13 @@ export class AdminGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.hasUser().pipe(
-      tap(user=> console.log(user)),
-      map(user => user === null ? false: true));
+      map(user => user === null ? false: true),
+      tap(hasUser=> {
+        if(!hasUser){
+          this.router.navigate(['/auth/login'])
+        }
+      }),
+      );
   }
 
 }
--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ProductsService } from "./../../../core/services/products/products.service";
 import { Router } from "@angular/router";
 import { MyValidators } from "./../../../utils/validators";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { finalize } from "rxjs/operators";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -12,11 +15,13 @@ import { MyValidators } from "./../../../utils/validators";
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private formBuilder:FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm()
    }
@@ -42,12 +47,30 @@ export class FormProductComponent implements OnInit {
     }
   }
 
+  uploadFile(event){
+    const file = event.target.files[0]
+    const name = 'image.png'
+    const fileRef = this.storage.ref(name)
+    const task = this.storage.upload(name, file)
+    
+    task.snapshotChanges()
+    .pipe(
+      finalize(()=>{
+        this.image$ = fileRef.getDownloadURL()
+        this.image$.subscribe(url=>{
+          this.form.get('image').setValue(url)
+        })
+      })
+    )
+    .subscribe()
+  }
+
   private buildForm(){
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
       price: ['', [Validators.required, MyValidators.isPriceValid]],
-      image: ['', [Validators.required]],
+      image: [''],
       description: ['', [Validators.required]],
     })
   }
