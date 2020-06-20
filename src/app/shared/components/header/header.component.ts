cfa6c92018ce3f7a0e2c089afdc916437ae14e60
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-20 17:08:11 -0500

    An button to install the app was added. The class: 'Probar funcionalidad del botón de instalacion' has finised.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { map } from "rxjs/operators";
 import { CartService } from "./../../../core/services/cart.service";
 import { Observable } from 'rxjs';
@@ -11,17 +11,32 @@ import { Observable } from 'rxjs';
 export class HeaderComponent implements OnInit {
 
   total$:Observable<number>
+  installEvent = null;
 
-  constructor(
-    private cartService: CartService
-  ) {
+  @HostListener('window: beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(event: Event){
+    event.preventDefault();
+    this.installEvent = event;
+  }
+
+  constructor(private cartService: CartService) {
     this.total$ = this.cartService.cart$
     .pipe(
       map(products => products.length)
     )
-   }
+  }
 
   ngOnInit() {
+  }
+
+  installByUser(){
+    if(this.installEvent){
+      this.installEvent.prompt();
+      this.installEvent.userChose
+      .then(rta=>{
+        
+      })
+    }
   }
 
 }
