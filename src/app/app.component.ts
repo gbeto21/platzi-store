c1da1ac069a1385e45aa4f30c740e59de170eb45
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-20 17:19:31 -0500

    The funtionaliti to auto update was created. The class: 'Enviando actualizaciones' has finished.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private swUpdate: SwUpdate){
+
+  }
+
+  ngOnInit(){
+    this.updatePWA();
+  }
+
+  updatePWA(){
+    this.swUpdate.available.subscribe(value =>{
+      window.location.reload();
+    });
+  }
+  
 }
